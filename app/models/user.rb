29c9c7596f079cf8556abb5bb78bc096aed8c2e6
jackiestchen <jackiestchen@gmail.com>
2022-04-20 00:08:11 -0600

    Feature: Add authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
-  validates :password, length: { minimum: 6}
+  validates :password, presence: true, length: { minimum: 6}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
-    if user && user.authenticate(password)
-      return user
+    user = User.find_by(email: email)&.authenticate(password)
+    if user  
+      user
     else 
-      return nil
+      nil
     end
   end
 
